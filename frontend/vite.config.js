d5db16e5f0c38a30bf69262719d917553adca707
zougmar <zougmar123@gmail.com>
2026-03-01 02:50:06 +0000

    c

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   server: {
     port: 3000,
     proxy: {
